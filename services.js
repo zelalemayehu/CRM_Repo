560adfa096f956a367f62dec3fcf7b11092538a0
Zelalem Gebre <zel.alem.ayehu@gmail.com>
2023-02-26 10:53:41 +0300

    Modify services.js

--- services.js
+++ services.js
@@ -17,17 +17,67 @@ app.post("/profile", (request, response) => {
         db.collection("Profile")
         .insertOne(data)
         .then(() => response.status(201).json({"message":"Profile is registered"}))
-        .catch(() => response.status(404).json({"error":"Failed to registered"}))
+        .catch(() => response.status(404).json({"error":"Profile Failed to registered"}))
         .finally(() => client.close())
     })
     .catch(() => response.status(404).json({"error": "DB_ERR"}));
 });
+
 //Register Contacts
-
+app.post("/Contact", (request, response) => { 
+    let data = request.body;
+    mongoClient.connect(DB_URL, {useNewUrlParser: true})
+    .then((client) => {
+        let db = client.db("mydb");
+        db.collection("Contact")
+        .insertOne(data)
+        .then(() => response.status(201).json({"message":"Contact is registered"}))
+        .catch(() => response.status(404).json({"error":" Contact Failed to registered"}))
+        .finally(() => client.close())
+    })
+    .catch(() => response.status(404).json({"error": "DB_ERR"}));
+});
 //Show all contacts
-
+app.get("/contacts", (request, response) => { 
+    mongoClient.connect(DB_URL, {useNewUrlParser:true})
+    .then((client) => {
+        let contacts = [];
+        let db = client.db("mydb");
+        let cursor = db.collection("Contact").find();
+        cursor.forEach(document => contacts.push(document))
+        .then(() => response.json(contacts))
+        .finally(() => client.close())
+    })
+    .catch(() => response.status(404).json({"error": "DB_ERR"}))
+});
 //Delete Contacts
+app.delete("/contact/:id", (request, response) => { 
+    let id = parseInt(request.params.id);
+   
+    mongoClient.connect(DB_URL, {useNewUrlParser: true})
+    .then((client) => {
+        let db = client.db("mydb");
+        db.collection("Contact")
+        .deleteOne({_id: id})
+        .then((value) => response.status(200).json(value))
+        .catch((err) => response.status(404).json(err))
+        .finally(() => client.close())
+    })
+    .catch(() => response.status(404).json({"error": "DB_ERR"}));
+});
 
 //Update Profile
-
-//
+app.put("/profile/:id", (request, response) => { 
+    let id = parseInt(request.params.id);
+    let data = request.body;
+    mongoClient.connect(DB_URL, {useNewUrlParser: true})
+    .then((client) => {
+        let db = client.db("mydb");
+        db.collection("Profile")
+        .updateOne({_id: id}, {$set: data})
+        .then((value) => response.status(200).json(value))
+        .catch((err) => response.status(404).json(err))
+        .finally(() => client.close())
+    })
+    .catch(() => response.status(404).json({"error": "DB_ERR"}));
+});
